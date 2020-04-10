6e83fb85634384fa0c108fa8a0f369e832eee24c
Sobhan1996 <mniknami@uwaterloo.ca>
2020-04-09 21:49:23 -0400

    Added path for the checkpoint manually

--- src/interactive_conditional_samples.py
+++ src/interactive_conditional_samples.py
@@ -64,7 +64,8 @@ def interact_model(
         )
 
         saver = tf.train.Saver()
-        ckpt = tf.train.latest_checkpoint(os.path.join('models', model_name))
+        # ckpt = tf.train.latest_checkpoint(os.path.join('models', model_name))
+        ckpt = tf.train.latest_checkpoint('./models/model_checkpoint')
         print(model_name)
         print(os.path.join('models', model_name))
         print(ckpt)
